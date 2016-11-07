9d90372b96d124004d25d0e4b79dcc9261ac547e
Nicolaj Evers <nicolajevers@hotmail.com>
2016-11-07 08:57:36 +0100

    Moved the camera to the new git

---
+++ DestructionGame/Assets/Scripts/Camera/CameraController6.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController6 : MonoBehaviour
+{
+
+    [Tooltip("The smoothness of the camera movement")]
+    [SerializeField]
+    private int smoothness = 3;
+    [Tooltip("The smoothness of the camera rotation")]
+    [SerializeField]
+    private int rotSmooth = 3;
+
+    private GameObject player;
+    private GameObject tempTrans;
+
+    private Vector3 offset;
+    private Quaternion offsetRotation;
+
+    private RaycastHit hit;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        offset = transform.position - player.transform.position;
+        offsetRotation = transform.rotation;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = player.transform.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, Time.deltaTime * smoothness);
+        transform.rotation = offsetRotation;
+
+        Physics.Linecast(player.transform.position, transform.position, out hit);
+        if (hit.transform != null)
+        {
+            if (hit.transform != transform)
+            {
+                Vector3 direction = player.transform.position - transform.position;
+                Quaternion rotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotSmooth);
+                transform.position = new Vector3(player.transform.position.x, transform.position.y, hit.transform.position.z);
+            }
+        }
+    }
+}
