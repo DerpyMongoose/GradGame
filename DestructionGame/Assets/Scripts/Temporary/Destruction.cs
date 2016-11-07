a9c4eadbf3144d59834bb056c4262ff458dfc6df
Nicolaj Evers <nicolajevers@hotmail.com>
2016-11-07 14:16:42 +0100

    Bad physics destruction

---
+++ DestructionGame/Assets/Scripts/Temporary/Destruction.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destruction : MonoBehaviour
+{
+    [SerializeField]
+    private int maxStates;
+    [SerializeField]
+    private GameObject prefab;
+    [SerializeField]
+    private int tPPrefab;
+
+    private GameObject player;
+    private ParticleSystem particleSys;
+
+    private int state;
+
+    // Use this for initialization
+    void Start()
+    {
+        state = 1;
+        particleSys = GetComponent<ParticleSystem>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        if(maxStates <= 0)
+        {
+            print("Please assign maxStates a value above 0");
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision col)
+    {
+
+        if(col.collider.gameObject == player)
+        {
+            col.collider.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            if(state == (maxStates - maxStates) + state)
+            {
+                print("state");
+                if (state >= maxStates)
+                {           
+                    for (int i = 0; i < tPPrefab; i++)
+                    {
+                        Instantiate(prefab,transform.position, Quaternion.identity);
+                    }
+                    Destroy(gameObject);
+                }
+
+                particleSys.startSize *= state; 
+                particleSys.Play();
+                state += state;
+            }
+        }
+    }
+}
