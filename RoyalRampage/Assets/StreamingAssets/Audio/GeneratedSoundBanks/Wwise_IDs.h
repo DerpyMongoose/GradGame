0ffd15c195a78b78a56e9ecf014913d56cd69c0e
Esben Gravesen <esben.gravesen@gmail.com>
2016-11-08 15:44:40 +0100

    Integrated Wwise project with placeholder sounds

---
+++ RoyalRampage/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,192 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIENCE_PLAY = 1496310610U;
+        static const AkUniqueID AMBIENCE_STOP = 3166394572U;
+        static const AkUniqueID DASH_PLAY = 2174485018U;
+        static const AkUniqueID MENU_BUTTON_PLAY = 4027780260U;
+        static const AkUniqueID MUSIC_SYSTEM_PLAY = 350081739U;
+        static const AkUniqueID MUSIC_SYSTEM_STOP = 3509289057U;
+        static const AkUniqueID OBJECT_ACTION_PLAY = 491891602U;
+        static const AkUniqueID OBJECTIVE_ANNOUNCED_PLAY = 453371149U;
+        static const AkUniqueID OBJECTIVE_COMPLETED_PLAY = 635791921U;
+        static const AkUniqueID POINTS_COUNTING_PLAY = 1311831249U;
+        static const AkUniqueID POINTS_COUNTING_STOP = 2563246215U;
+        static const AkUniqueID POINTS_REWARDED_PLAY = 3286289918U;
+        static const AkUniqueID SCORE_SCREEN_OPEN_PLAY = 2664777132U;
+        static const AkUniqueID SPIN_PLAY = 2848266490U;
+        static const AkUniqueID STAR_REWARDED_PLAY = 2485028023U;
+        static const AkUniqueID START_GAME_BUTTON_PLAY = 278413312U;
+        static const AkUniqueID STOMP_PLAY = 3839850873U;
+        static const AkUniqueID TIMER_TICK_PLAY = 3295885793U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace ENVIRONMENTS
+        {
+            static const AkUniqueID GROUP = 3761286811U;
+
+            namespace STATE
+            {
+                static const AkUniqueID HALL = 3633416828U;
+                static const AkUniqueID KITCHEN = 1586158131U;
+                static const AkUniqueID LIVING_ROOM = 3355232812U;
+            } // namespace STATE
+        } // namespace ENVIRONMENTS
+
+        namespace GAME_STATES
+        {
+            static const AkUniqueID GROUP = 2721494480U;
+
+            namespace STATE
+            {
+                static const AkUniqueID IN_INTRO_CUTSCENE = 2874168282U;
+                static const AkUniqueID IN_LEVEL = 1933493779U;
+                static const AkUniqueID IN_LEVEL_TIME_RUNNING_OUT = 3754610904U;
+                static const AkUniqueID IN_LEVEL_TIMES_UP = 3976417512U;
+                static const AkUniqueID IN_MAIN_MENU = 1415091186U;
+                static const AkUniqueID IN_SCORE_SCREEN = 591173034U;
+            } // namespace STATE
+        } // namespace GAME_STATES
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace OBJECT_ACTIONS
+        {
+            static const AkUniqueID GROUP = 1870950152U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DESTRUCTION = 2446376615U;
+                static const AkUniqueID LANDING = 2548270042U;
+            } // namespace SWITCH
+        } // namespace OBJECT_ACTIONS
+
+        namespace OBJECTS
+        {
+            static const AkUniqueID GROUP = 1695690031U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BARREL = 442447909U;
+                static const AkUniqueID BASKET = 1931853987U;
+                static const AkUniqueID BOOK = 1761501022U;
+                static const AkUniqueID BOTTLE = 3848582449U;
+                static const AkUniqueID CHAIR = 48383104U;
+                static const AkUniqueID CHINA = 4260845482U;
+                static const AkUniqueID CRATE = 3098439042U;
+                static const AkUniqueID CUTLERY = 1169658065U;
+                static const AkUniqueID FOOD = 3031504781U;
+                static const AkUniqueID GLASS = 2449969375U;
+                static const AkUniqueID GLOBE = 2867835716U;
+                static const AkUniqueID GRAND_PIANO = 1892659287U;
+                static const AkUniqueID GRANDFATHER_CLOCK = 2995585464U;
+                static const AkUniqueID LAMP = 657744959U;
+                static const AkUniqueID PLANT = 114672780U;
+                static const AkUniqueID PORCELAIN = 3020931316U;
+                static const AkUniqueID POT_AND_PAN = 2200909088U;
+                static const AkUniqueID SERVING_TRAY = 333711018U;
+                static const AkUniqueID SHELF = 3086965867U;
+                static const AkUniqueID STATUE = 939730917U;
+                static const AkUniqueID SUIT_OF_ARMOR = 4241762846U;
+                static const AkUniqueID TABLE = 2048817515U;
+                static const AkUniqueID TROPHY = 762946915U;
+                static const AkUniqueID WHEELED_TRAY = 716963392U;
+            } // namespace SWITCH
+        } // namespace OBJECTS
+
+        namespace STARS
+        {
+            static const AkUniqueID GROUP = 1281810928U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BONUS = 2356001030U;
+                static const AkUniqueID NORMAL = 1160234136U;
+            } // namespace SWITCH
+        } // namespace STARS
+
+        namespace SURFACES
+        {
+            static const AkUniqueID GROUP = 3714303081U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CARPET = 2412606308U;
+                static const AkUniqueID MARBLE = 1127618254U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace SURFACES
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID IMPACT_VELOCITY = 238739503U;
+        static const AkUniqueID LEVEL_TIME = 2699314947U;
+        static const AkUniqueID POINT_COUNTER = 5057028U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+        static const AkUniqueID STAR_COUNTER = 4112747480U;
+    } // namespace GAME_PARAMETERS
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID NO_STAR_PROGRESS_FANFARE = 7987325U;
+        static const AkUniqueID STAR_PROGRESS_FANFARE = 724164523U;
+    } // namespace TRIGGERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID CHARACTER = 436743010U;
+        static const AkUniqueID ENVIRONMENTAL = 1973600711U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID OBJECTS = 1695690031U;
+        static const AkUniqueID REVERB = 348963605U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID CORRIDOR = 4063189299U;
+        static const AkUniqueID HALL = 3633416828U;
+        static const AkUniqueID ROOM_HARD = 2608754132U;
+        static const AkUniqueID ROOM_SOFT = 2222662711U;
+    } // namespace AUX_BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
