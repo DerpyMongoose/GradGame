977d666721b90492766e65e5f4287fe69b07d7cb
Nicolaj Evers <nicolajevers@hotmail.com>
2016-11-10 14:50:45 +0100

    Hard fix for objectbehavior

---
+++ RoyalRampage/Assets/Scripts/Temporary/ObjectBehavior.cs
@@ -0,0 +1,242 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectBehavior : MonoBehaviour
+{
+    private GameObject rubblePrefab;
+    private int life;
+    private int rubbleAmount;
+    private int state;
+
+    private Vector3 initialPos;
+
+    private Rigidbody objRB;
+    private GameObject player;
+    private ParticleSystem particleSys;
+    private IEnumerator coroutine;
+
+    [HideInInspector]
+    public bool isGrounded = true;
+    private bool hit = false;
+    private bool readyToCheck;
+    private bool lifted;
+
+    private float checkHeight;
+
+    [HideInInspector]
+    public int score;
+
+
+    //object name to be used for Quest system??
+    public enum DestructableObject
+    {
+        BARREL, CHAIR, TABLE, WARDROBE, BED, BOX
+    }
+
+    public DestructableObject objType;
+
+    // Use this for initialization
+    //void OnEnable()
+    //{
+    //    GameManager.stampPower += Lift;
+    //}
+
+    //void OnDisable()
+    //{
+    //    GameManager.stampPower -= Lift;
+    //}
+
+    void Start()
+    {
+        //coroutine = Wait();
+        //StartCoroutine(coroutine);
+        switch (objType)
+        {
+            case DestructableObject.BARREL:
+                score = ObjectManager.instance.barrelScore;
+                life = ObjectManager.instance.barrelLife;
+                rubbleAmount = ObjectManager.instance.barrelRubbleAmount;
+                rubblePrefab = ObjectManager.instance.barrelRubblePrefab;
+                break;
+            case DestructableObject.BED:
+                score = ObjectManager.instance.bedScore;
+                life = ObjectManager.instance.bedLife;
+                rubbleAmount = ObjectManager.instance.bedRubbleAmount;
+                rubblePrefab = ObjectManager.instance.bedRubblePrefab;
+                break;
+            case DestructableObject.BOX:
+                score = ObjectManager.instance.boxScore;
+                life = ObjectManager.instance.boxLife;
+                rubbleAmount = ObjectManager.instance.boxRubbleAmount;
+                rubblePrefab = ObjectManager.instance.boxRubblePrefab;
+                break;
+            case DestructableObject.CHAIR:
+                score = ObjectManager.instance.chairScore;
+                life = ObjectManager.instance.chairLife;
+                rubbleAmount = ObjectManager.instance.chairRubbleAmount;
+                rubblePrefab = ObjectManager.instance.chairRubblePrefab;
+                break;
+            case DestructableObject.TABLE:
+                score = ObjectManager.instance.tableScore;
+                life = ObjectManager.instance.tableLife;
+                rubbleAmount = ObjectManager.instance.tableRubbleAmount;
+                rubblePrefab = ObjectManager.instance.tableRubblePrefab;
+                break;
+            case DestructableObject.WARDROBE:
+                score = ObjectManager.instance.wardrobeScore;
+                life = ObjectManager.instance.wardrobeLife;
+                rubbleAmount = ObjectManager.instance.wardrobeRubbleAmount;
+                rubblePrefab = ObjectManager.instance.wardrobeRubblePrefab;
+                break;
+            default:
+                break;
+
+        }
+        state = 1;
+        particleSys = GetComponent<ParticleSystem>();
+        objRB = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        initialPos = transform.position;
+    }
+
+
+    void Update()
+    {
+        //print(Mathf.Round(GetComponent<Rigidbody>().velocity.y * 10) / 10);
+        if (GameManager.instance.player.GetComponent<PlayerStates>().lifted)
+        {
+            if (Mathf.Round(GetComponent<Rigidbody>().velocity.y * 10) / 10 < 0)
+            {
+                GameManager.instance.player.GetComponent<PlayerStates>().imInSlowMotion = true;
+                //checkHeight = 0;
+                GetComponent<Rigidbody>().useGravity = false;
+                //lifted = true;
+                coroutine = ReturnGravity();
+                StartCoroutine(coroutine);
+            }
+        }
+
+        if(Mathf.Round(GetComponent<Rigidbody>().velocity.y * 10) / 10 >= 0)
+        {
+            //GameManager.instance.player.GetComponent<PlayerStates>().lifted = false;
+        }
+
+        //if (GameManager.instance.player.GetComponent<PhysicalMovement>().ableToLift)
+        //{
+        //    lifted = false;
+        //}
+        //else
+        //{
+        //    //print("came into");
+        //    Rigidbody rig = GetComponent<Rigidbody>();
+        //    if (Mathf.Round(transform.position.y * 10) / 10 >= checkHeight && !lifted)
+        //    {
+
+        //        checkHeight = Mathf.Round(transform.position.y * 10) / 10;
+        //    }
+        //    else if (Mathf.Round(transform.position.y * 10) / 10 < checkHeight && readyToCheck)
+        //    {
+        //        GameManager.instance.player.GetComponent<PlayerStates>().imInSlowMotion = true;
+        //        checkHeight = 0;
+        //        GetComponent<Rigidbody>().useGravity = false;
+        //        lifted = true;
+        //        coroutine = ReturnGravity();
+        //        StartCoroutine(coroutine);
+        //    }
+        //}
+
+
+        if (!GameManager.instance.player.GetComponent<PlayerStates>().imInSlowMotion)
+        {
+            GetComponent<Rigidbody>().useGravity = true;
+        }
+        if(!Mathf.Approximately(initialPos.y, transform.position.y))
+        {
+            isGrounded = false;
+        }
+    }
+
+    void DestroyObj(GameObject obj)
+    {
+        for (int i = 0; i < obj.GetComponent<ObjectBehavior>().rubbleAmount; i++)
+        {
+            Instantiate(obj.GetComponent<ObjectBehavior>().rubblePrefab, obj.transform.position, Quaternion.identity);
+        }
+        GameManager.instance.objectDestructed(obj);
+        Destroy(obj);
+    }
+
+    //IEnumerator Wait()
+    //{
+    //    yield return new WaitForSeconds(Time.deltaTime * 2);
+    //    checkHeight = Mathf.Round(transform.position.y * 10) / 10;
+    //    readyToCheck = true;
+    //}
+
+    IEnumerator ReturnGravity()
+    {
+		yield return new WaitForSeconds(GameManager.instance.player.GetComponent<PlayerStates>().gravityTimer);
+		GameManager.instance.player.GetComponent<PlayerStates>().imInSlowMotion = false;
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        if (col.collider.gameObject == player)
+        {
+            hit = true;
+            //Damage system, it takes more hits to destroy
+            /*if(state == (life - life) + state)
+            {
+                if (state >= life)
+                {           
+                    for (int i = 0; i < rubbleAmount; i++)
+                    {
+                        Instantiate(rubblePrefab,transform.position, Quaternion.identity);
+                    }
+					if()
+                    {
+
+                    }
+                }*/
+            if (particleSys != null)
+            {
+                particleSys.startSize *= state;
+                particleSys.Play();
+            }
+            /*state += 1;
+        }*/
+        }
+        if (col.collider.tag == "Destructable" && hit == true)
+        {
+            if (isGrounded == false && col.gameObject.GetComponent<ObjectBehavior>().isGrounded == false)
+            {
+                DestroyObj(gameObject);
+            }
+            if (isGrounded == false)
+            {
+                DestroyObj(gameObject);
+            }
+            if (isGrounded == true)
+            {
+                DestroyObj(gameObject);
+                DestroyObj(col.gameObject);
+            }
+        }
+
+        if (col.collider.tag == "Wall") {
+
+            col.collider.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+        if (col.collider.tag == "Floor" || objRB.velocity == Vector3.zero)
+        {
+            isGrounded = true;
+            initialPos = transform.position;
+        }
+    }
+}
