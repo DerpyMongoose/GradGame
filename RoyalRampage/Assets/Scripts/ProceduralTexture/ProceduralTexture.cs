a870c304d1677ee1519a0b3f77458e7596143518
Christian Andersen <Chr4000@hotmail.com>
2016-11-10 11:20:52 +0100

    DestructionAsset

---
+++ RoyalRampage/Assets/Scripts/ProceduralTexture/ProceduralTexture.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProceduralTexture : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+
+        Texture2D newTex = new Texture2D(200, 200);
+        for (int i = 0; i <= 100; i++) {
+            for(int j = 0; j <= 100; j++) {
+                newTex.SetPixel(j, i, Color.blue);
+            }
+        }
+
+        for (int i = 101; i <= 200; i++) {
+            for (int j = 101; j <= 200; j++) {
+                newTex.SetPixel(j, i, Color.green);
+            }
+        }
+
+        newTex.Apply();
+
+        GetComponent<Renderer>().material.mainTexture = newTex;
+    }
+	
+}
