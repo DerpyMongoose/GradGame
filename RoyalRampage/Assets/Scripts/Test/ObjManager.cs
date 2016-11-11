ee2b2eceb427947a32939c603f68be802ff0e12a
Nicolaj Evers <nicolajevers@hotmail.com>
2016-11-11 13:38:18 +0100

    Camera changes + ObjectManager testing

---
+++ RoyalRampage/Assets/Scripts/Test/ObjManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjManager : MonoBehaviour
+{
+    public enum type
+    {
+        BARREL, BED, BOX
+    }
+
+    public type type_;
+
+    public List<DestructableObject> Objects = new List<DestructableObject>(0);
+
+    void Start()
+    {
+
+    }
+}
+
