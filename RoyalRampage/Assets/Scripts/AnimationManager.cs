cc52da3b73c4ec692a8a7bd32032a20640dc2dea
Nicolaj Evers <nicolajevers@hotmail.com>
2016-11-15 16:04:01 +0100

    * animation stuff

--- RoyalRampage/Assets/Scripts/AnimationManager.cs
+++ RoyalRampage/Assets/Scripts/AnimationManager.cs
@@ -3,12 +3,36 @@ using System.Collections;
 
 public class AnimationManager : MonoBehaviour {
 
+
+    // animation variables
+    private bool isSpinning = false;
+    private bool isIdle = false;
+    private bool isStomping = false;
+    private bool isDashing = false;
+
+    private Animator playerAnim,
+        scepterAnim;
+    
+    void PlayerDashAnim()
+    {
+
+        //playerAnim.SetBool("is_dashing", true);
+        playerAnim.SetTrigger("dash_trig");
+        scepterAnim.SetTrigger("scepterdash_trig");
+
+    }
+
 	void OnEnable(){
+        playerAnim = GameManager.instance.player.transform.GetChild(0).GetComponent<Animator>();
+        scepterAnim = GameObject.FindGameObjectWithTag("Scepter").GetComponent<Animator>();
+        print(scepterAnim);
+
+        GameManager.instance.OnPlayerDash += PlayerDashAnim;
 
 	}
 
 	void OnDisable(){
-
-	}
+        GameManager.instance.OnPlayerDash -= PlayerDashAnim;
+    }
 
 }
