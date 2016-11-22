e6f922fdf639245d5fcd37eb4ca61e9e162b61a0
activeCG <sivers12@student.aau.dk>
2016-11-22 14:37:05 +0100

    * test tools

---
+++ RoyalRampage/Assets/Scripts/AutoPowerupScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoPowerupScript : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter (Collider other) {
+		var scale = other.transform.localScale;
+		other.transform.localScale = scale * 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
