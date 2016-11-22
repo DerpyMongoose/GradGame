63c5b04adf9aaaf51f8e4a82153062ec8a2c6376
Christian Andersen <Chr4000@hotmail.com>
2016-11-22 09:46:20 +0100

    BugFixing menu stuff

---
+++ RoyalRampage/Assets/Scripts/ButtonClickedScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonClickedScript : MonoBehaviour {
+
+    Color pressed = new Color(0.97f, 0.76f,0.15f);
+    Color notPressed = new Color(0.79f, 0.75f, 0.63f);
+    public int index;
+	// Use this for initialization
+	void Start () {
+	    if(index == LanguageManager.instance.languageSelect) {
+            GetComponent<Image>().color = pressed;
+        } else {
+            GetComponent<Image>().color = notPressed;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (index == LanguageManager.instance.languageSelect) {
+            GetComponent<Image>().color = pressed;
+        } else {
+            GetComponent<Image>().color = notPressed;
+        }
+    }
+}
