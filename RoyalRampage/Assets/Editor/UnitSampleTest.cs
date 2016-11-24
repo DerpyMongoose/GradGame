6d52d6dce07c01138cd51f6348d8a9c391b5ed4e
activeCG <sivers12@student.aau.dk>
2016-11-24 11:26:23 +0100

    * unitsampletest

--- RoyalRampage/Assets/Editor/UnitSampleTest.cs
+++ RoyalRampage/Assets/Editor/UnitSampleTest.cs
@@ -2,18 +2,17 @@
 using NUnit.Framework;
 using UnityEngine;
 
-[TestFixture]
-public class UnitSampleTest {
 
-	[Test]
-	public void SimpleCamTest(){
-		//Assert.That (2 + 2 == 4);
-		var test = GameObject.FindObjectOfType<AutoPowerupScript>() as AutoPowerupScript;
-		Assert.That (test.findSth()!=null);
+namespace UnityTest{
+	[TestFixture]
+	internal class UnitSampleTest {
 
-	}
+		[Test]
+		public void SimpleCamTest(){
+			//Assert.That (2 + 2 == 4);
+			var test = GameObject.FindObjectOfType<AutoPowerupScript>() as AutoPowerupScript;
+			Assert.That (test.findSth()!=null);
 
-	public void RunUnitTest(){
-		SimpleCamTest ();
+		}
 	}
 }
