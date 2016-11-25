b66970b41e7c899ab2e6e1624d6b3c29bc550fcb
Christian Andersen <Chr4000@hotmail.com>
2016-11-25 10:17:48 +0100

    Make point objects and object pool

---
+++ RoyalRampage/Assets/Scripts/PointObject/PointObjectPool.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class PointObjectPool : MonoBehaviour {
+
+    //Singleton
+    public static PointObjectPool instance;
+
+    //List for the points (pool)
+    public List<GameObject> points;
+
+    //The Gameobject points
+    public GameObject pointObject;
+
+    //How many in the pool
+    public int amountInPool;
+
+    //Is the pool too small?
+    bool willGrow = true;
+
+    //Create Singleton
+    void Awake() {
+        if(instance == null) {
+            instance = this;
+        }
+    }
+
+	// Create pool on start
+	void Start () {
+        points = new List<GameObject>();
+
+        for (int i = 0; i < amountInPool; i++) {
+            GameObject obj = (GameObject)Instantiate(pointObject);
+            obj.SetActive(false);
+            points.Add(obj);
+        }
+	}
+	
+    //Get Object From pool
+    public GameObject FindObjectInPool() {
+        for (int i = 0; i < points.Count; i++) {
+            if (!points[i].activeInHierarchy) {
+                return points[i];
+            }
+        }
+
+        if (willGrow) {
+            for (int i = 0; i < 4; i++) {
+                GameObject obj = (GameObject)Instantiate(pointObject);
+                points.Add(obj);
+            }
+            GameObject lastObj = (GameObject)Instantiate(pointObject);
+            points.Add(lastObj);
+            return lastObj;
+        }
+
+        return null;
+    }
+}
---
+++ RoyalRampage/Assets/Scripts/PointObject/SetTextPoints.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetTextPoints : MonoBehaviour {
+
+	
+	void OnEnable () {
+	
+	}
+	
+    void SetText(int pointAmount) {
+
+    }
+}
