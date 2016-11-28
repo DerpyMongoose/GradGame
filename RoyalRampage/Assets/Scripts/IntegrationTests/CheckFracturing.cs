179078579080bfc74d7a739fae863983b867a2e5
activeCG <sivers12@student.aau.dk>
2016-11-28 13:11:59 +0100

    * check whether small objects don't have the fracturing scripts

---
+++ RoyalRampage/Assets/Scripts/IntegrationTests/CheckFracturing.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckFracturing : MonoBehaviour {
+
+	public int num_of_fractured_objects = 0;
+	private FracturedObject [] fractured;
+
+	void Start () {
+		fractured = GameObject.FindObjectsOfType<FracturedObject>();
+		num_of_fractured_objects = fractured.Length;
+		Debug.Log (fractured.Length);
+
+		if(num_of_fractured_objects > 0)
+			GetComponent<CheckFracturing> ().enabled = false;
+	}
+}
