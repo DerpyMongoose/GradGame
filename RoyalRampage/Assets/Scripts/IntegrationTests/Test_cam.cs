0e510c6ab233711a841bfcd9cfc89fd3b859dad1
activeCG <sivers12@student.aau.dk>
2016-11-28 10:14:25 +0100

    * camera test done

---
+++ RoyalRampage/Assets/Scripts/IntegrationTests/Test_cam.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test_cam : MonoBehaviour {
+	
+	Transform Player;
+
+	void Start(){
+		Player = GameManager.instance.player.transform;
+	}
+	void Update () {
+		if(Player.position.x >= -3.5){
+			Player.position += new Vector3(-0.3f*Time.deltaTime,0,0);
+		}
+	}
+}
