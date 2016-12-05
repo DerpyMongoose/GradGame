2aad7be4128e8458200aca3d20e86fb4a14d55e6
activeCG <sivers12@student.aau.dk>
2016-12-05 15:38:18 +0100

    * testing DONE

--- RoyalRampage/Assets/Scripts/IntegrationTests/liftMassTest.cs
+++ RoyalRampage/Assets/Scripts/IntegrationTests/liftMassTest.cs
@@ -11,6 +11,7 @@ public class liftMassTest : MonoBehaviour {
 	}
 
 	void Update () {
+		//if hasn't run the stomp, run now
 		if (hasStomped != true){
 			StompTest ();
 			hasStomped = true;
@@ -18,13 +19,15 @@ public class liftMassTest : MonoBehaviour {
 	}
 
 	void StompTest(){
+		//disable level canvas to see the level
 		GameObject.Find("InLevelCanvas").SetActive(false);
 
+		//prepare arrays to save masse, colliders, rigidbodies
 		List<Collider> colliders = new List<Collider>();
 		List<Rigidbody> rbs = new List<Rigidbody>();
 		List<float> masses = new List<float>();
 
-		GameObject Player;
+		//find all objects to lifted on stomp (code copied from original stomp function)
 		Collider[] hitColliders = null;
 		hitColliders = Physics.OverlapSphere(transform.position, player.GetComponent<PlayerStates>().liftRadius);
 		//Lift(hitColliders); //RUN FROM ANIMATION EVENT
@@ -34,14 +37,17 @@ public class liftMassTest : MonoBehaviour {
 				colliders.Add(hitColliders[i]);
 			}
 		}
+
+		//update tempcolliders (objects to lift) in alex's swipe half script 
 		player.GetComponent<SwipeHalf> ().tempColliders = colliders;
+		//save initial rigidbody masses for later check
 		for(int i = 0 ; i < colliders.Count; i++){
 			rbs.Add(colliders [i].gameObject.GetComponent<Rigidbody> ());
 			masses.Add (rbs [i].mass);
 		}
-
+		//lift objects
 		player.GetComponent<SwipeHalf> ().Lift ();
-
+		//check whether masses changed to 1
 		for(int i = 0 ; i < colliders.Count; i++){
 			if(colliders[i].GetComponent<Rigidbody>().mass != 1.0f){
 				GetComponent<liftMassTest> ().enabled = false;
@@ -49,9 +55,10 @@ public class liftMassTest : MonoBehaviour {
 			}
 		}
 
-		//vfall
+		//call reverse function to let objects fall down back
 		player.GetComponent<SwipeHalf> ().Reverse (rbs,masses);
 
+		//check whether mases reset to initial, compare to masses array that we saved earlier
 		for(int i = 0 ; i < colliders.Count; i++){
 			if(colliders[i].GetComponent<Rigidbody>().mass == 1.0f){
 				GetComponent<liftMassTest> ().enabled = false;
